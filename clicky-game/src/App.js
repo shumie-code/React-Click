f5384812c3be406108c2622b40565f7d23f0b282
Petros Shumie <shumie.peter@gmail.com>
2020-01-20 16:08:43 -0500

    Added imports to inital  App.js file.

--- clicky-game/src/App.js
+++ clicky-game/src/App.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import 
-
-class App  extends Component {
-
- 
+import { Row, CardPanel, Col } from 'react-materialize';
+import Nav from './components/Nav/Nav';
+import Game from './Components/Game';
