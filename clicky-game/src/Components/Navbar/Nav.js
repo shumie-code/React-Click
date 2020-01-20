53a500178b3e241d392063718074483fdc90466c
Petros Shumie <shumie.peter@gmail.com>
2020-01-20 17:12:57 -0500

    Wrote render method to pass props to list items if correct answer has been submitted, render method also keeps track of current score on list item and checks for high score.

--- clicky-game/src/Components/Navbar/Nav.js
+++ clicky-game/src/Components/Navbar/Nav.js
@@ -20,7 +20,24 @@ class Nav extends Component {
 
         window.clearTimeout(this.timeout)
         if (!clear & correct !== undefined) {
-            this.timeout = window.setTimeout(this.renderMessage, gameWon)
+            this.timeout = window.setTimeout(this.renderMessage, gameWon ? 3000 : 1000, gameWon ? undefined : correct, false, true)
         }
+        return <li key={Math.random()} className={className}>{message}</li>
+    }
+
+    render() {
+        return ( 
+            <nav className="pinned">
+                <div>
+                    <ul className="center navList">
+                        <li className="logo">Clicky Game</li>
+                        {this.renderMessage(this.props.correct, this.props.gameWon)}
+                        <li>Score: {this.props.score} | High Score: {this.props.highScore}</li>
+                    </ul>
+                </div>
+            </nav>
+        )
     }
 }
+
+export default Nav
