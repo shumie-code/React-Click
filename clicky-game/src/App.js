c81cfdaae2d738d299db42d08acda7e958e9dd93
Petros Shumie <shumie.peter@gmail.com>
2020-01-20 16:31:24 -0500

    Created render method for deciding ig the current score should changed based on the current state and be reflected in the navbar. Decided whether the game answer is correct and modifies the current state.

--- clicky-game/src/App.js
+++ clicky-game/src/App.js
@@ -2,9 +2,9 @@ import React, { Component } from 'react';
 import { Row, CardPanel, Col } from 'react-materialize';
 import Nav from './components/Nav/Nav';
 import Game from './Components/Game';
-import Imgs from '../public/imgs';
+import Imgs from '../public/Imgs';
 
-// Initalize App component state 
+// Initialize App component state 
 class App extends Component {
   state = {
     images: [],
@@ -20,7 +20,7 @@ random = (a, b) => Math.random() > .5 ? -1 : 1
 
 // Event handler for images clicked
 clickHandler = image => {
-  if (this.state.clicked.indexOf(images) === -1 {
+  if (this.state.clicked.indexOf(images) === -1) {
     let score = this.state.clicked.length + 1,
     clicked = score === this.state.images.length ? [] : [...this.state.clicked, imageName]
 
@@ -34,8 +34,34 @@ clickHandler = image => {
     })
   } else {
     this.setState({
-      
+      images: this.state.images.sort(this.random),
+      clicked: [],
+      score: 0,
+      correct: false,
+      gameWon: false
     })
-  })
+  }
+}
+
+render() {
+  return (
+    <div>
+      <div className="container-fluid">
+        <Nav correct={this.state.correct} gameWon={this.state.gameWon} score={this.state.score} highScore={this.state.highScore} />
+        <Row>
+          <Col s={12}>
+            <CardPanel>
+              <p>CLick on a team LOGO to up your score, BEWARE! Do not click the same image twice!</p>
+            </CardPanel>
+          </Col>
+        </Row>
+      </div>
+    <div className="container">
+      <Row>
+        {this.state.images.map(images => <Game correct={this.state.correct} key={images.logoName} images={images} clickHandler={this.clickHandler} />)}
+      </Row>
+      </div>
+    </div>
+  )
 }
 }
