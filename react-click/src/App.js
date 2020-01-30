3fdb15a47f95d791dbd90bc5b0bebbd6ce801be4
Petros Shumie <shumie.peter@gmail.com>
2020-01-30 15:27:41 -0500

    Added shuffle array method to handle logic of rearranging logos

--- react-click/src/App.js
+++ react-click/src/App.js
@@ -26,5 +26,16 @@ class App extends Component {
     shuffleLogos = () => {
         this.setState(this.state.logos = this.shuffleArray(this.state.logos))
     }
+
+    shuffleArray = (a) => {
+        var j, x, i;
+        for (i = a.length -1; i > 0; i--) {
+            j = Math.floor(Math.random() * (i + 1));
+            x = a[i];
+            a[i] = a[j];
+            a[j] = x;
+        }
+        return a;
+    }
     
 }
